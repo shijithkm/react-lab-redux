944e988495cee38bf8f6351c62a56244a269d5d6
shijithkm <developer.shijith@gmail.com>
2020-04-11 21:21:02 +0530

    Redux configured

--- redux/actions/courseActions.js
+++ redux/actions/courseActions.js
@@ -1,4 +1,6 @@
+import * as types from './actionTypes';
+
 export function createCourse(course){
   console.log('Action',course);
-  return { type:"CREATE_COURSE",course} // course:course Object short hand syntax
+  return { type:types.CREATE_COURSE,course} // course:course Object short hand syntax
 }
--- redux/reducers/courseReducer.js
+++ redux/reducers/courseReducer.js
@@ -1,7 +1,9 @@
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state=[],action){
   console.log('reducer',action);
   switch(action.type){
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       return [...state,{...action.course}]
     default: 
       return state;
